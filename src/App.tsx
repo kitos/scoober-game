fdde5b13f343cec80a41535423cb9991492a2307
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2018-12-26 22:37:09 +0300

    feat: :sparkles: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
 import * as React from 'react'
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components'
 
-let App = () => <h1>Scoober game</h1>
+let colors = {
+  blue: '#65A9D8'
+}
+
+let theme = { colors }
+
+let GlobalStyle = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+`
+
+let Header = styled.header`
+  background: ${({ theme }) => theme.colors.blue};
+  color: #fff;
+  padding: 10px 20px;
+`
+
+let AppName = styled.h1`
+  margin: 0 0 5px;
+  font-weight: normal;
+`
+
+let App = () => (
+  <ThemeProvider theme={theme}>
+    <>
+      <GlobalStyle />
+
+      <Header>
+        <AppName>Scoober team</AppName>
+
+        <div>Win the game or win the job</div>
+      </Header>
+    </>
+  </ThemeProvider>
+)
 
 export default App
